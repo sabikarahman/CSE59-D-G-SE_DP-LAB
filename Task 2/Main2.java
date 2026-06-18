039e1257f899bd011400334ffa2a4b9d311c511e
sabikarahman <sabikarahman2003@gmail.com>
2026-06-18 16:39:02 +0600

    Add NumberChecker class to check even and greater than 10

--- Task 2/Main2.java
+++ Task 2/Main2.java
@@ -1,1 +1,18 @@
+package com.mycompany.main1;
+
+class NumberChecker {
+void check(int num) {
+       
+ boolean result = (num % 2 == 0) && (num > 10);
+  System.out.println(result);
+    }
+}
+
+    public class main2 {
+    public static void main(String[] args) {
+        
+     NumberChecker checker = new NumberChecker();
+     checker.check(14);
+    }
+}
 
