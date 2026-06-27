c699e2b4ff33e4d0ed40c75fe61291854f306f3e
sabikarahman <sabikarahman2003@gmail.com>
2026-06-28 00:00:47 +0600

    Implement shape classes and area calculation

--- Task 3/Main2.java
+++ Task 3/Main2.java
@@ -1,1 +1,72 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Shape[] shapes = {
+            new Rectangle(5, 4),
+            new Circle(3),
+            new Triangle(6, 2)
+        };
+
+        AreaCalculator calculator = new AreaCalculator();
+        System.out.println("Total Area = " + calculator.calculateTotalArea(shapes));
+    }
+}
+
+       interface Shape {
+       double calculateArea();
+}
+
+    class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+}
+
+
+class Circle implements Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+    class Triangle implements Shape {
+    private double base;
+    private double height;
+
+    public Triangle(double base, double height) {
+        this.base = base;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+}
+
+   class AreaCalculator {
+   public double calculateTotalArea(Shape[] shapes) {
+        double totalArea = 0;
+
+        for (Shape shape : shapes) {
+            totalArea += shape.calculateArea();
+        }
+
+        return totalArea;
+    }
+}
